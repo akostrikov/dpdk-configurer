c4e3e930f233124ee77ceb09a3ab19f705d34138
Alexandr Kostrikov <alexandr.kostrikov@gmail.com>
2018-05-23 16:47:09 +0300

    Run gofmt

--- dpdk-configurer.go
+++ dpdk-configurer.go
@@ -1,63 +1,62 @@
 package main
 
 import (
-	"html/template"
-	"io/ioutil"
-	"golang.org/x/crypto/ssh"
 	"bufio"
 	"bytes"
+	"errors"
 	"fmt"
+	"golang.org/x/crypto/ssh"
+	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
-	"errors"
 )
 
 func getHostKey(host string) (ssh.PublicKey, error) {
-    file, err := os.Open(filepath.Join(os.Getenv("HOME"), ".ssh", "known_hosts"))
-    if err != nil {
-        return nil, err
-    }
-    defer file.Close()
+	file, err := os.Open(filepath.Join(os.Getenv("HOME"), ".ssh", "known_hosts"))
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
 
-    scanner := bufio.NewScanner(file)
-    var hostKey ssh.PublicKey
-    for scanner.Scan() {
-        fields := strings.Split(scanner.Text(), " ")
-        if len(fields) != 3 {
-            continue
-        }
-        if strings.Contains(fields[0], host) {
-            var err error
-            hostKey, _, _, _, err = ssh.ParseAuthorizedKey(scanner.Bytes())
-            if err != nil {
-                return nil, errors.New(fmt.Sprintf("error parsing %q: %v", fields[2], err))
-            }
-            break
-        }
-    }
+	scanner := bufio.NewScanner(file)
+	var hostKey ssh.PublicKey
+	for scanner.Scan() {
+		fields := strings.Split(scanner.Text(), " ")
+		if len(fields) != 3 {
+			continue
+		}
+		if strings.Contains(fields[0], host) {
+			var err error
+			hostKey, _, _, _, err = ssh.ParseAuthorizedKey(scanner.Bytes())
+			if err != nil {
+				return nil, errors.New(fmt.Sprintf("error parsing %q: %v", fields[2], err))
+			}
+			break
+		}
+	}
 
-    if hostKey == nil {
-        return nil, errors.New(fmt.Sprintf("no hostkey for %s", host))
-    }
-    return hostKey, nil
+	if hostKey == nil {
+		return nil, errors.New(fmt.Sprintf("no hostkey for %s", host))
+	}
+	return hostKey, nil
 }
-
 
 func renderConnectFormHandler(w http.ResponseWriter, r *http.Request) {
-    t, _ := template.ParseFiles("connect_form.html")
-    t.Execute(w,nil)
+	t, _ := template.ParseFiles("connect_form.html")
+	t.Execute(w, nil)
 }
 
-func connectHandler(w http.ResponseWriter, r *http.Request){
+func connectHandler(w http.ResponseWriter, r *http.Request) {
 	var hostKey ssh.PublicKey
 	address := r.FormValue("address")
 
 	hostKey, err := getHostKey(address)
 	if err != nil {
-	    log.Fatal(err)
+		log.Fatal(err)
 	}
 
 	key, err := ioutil.ReadFile("/root/.ssh/id_rsa")
@@ -77,7 +76,7 @@ func connectHandler(w http.ResponseWriter, r *http.Request){
 		HostKeyCallback: ssh.FixedHostKey(hostKey),
 	}
 
-	client, err := ssh.Dial("tcp", address + ":22", config)
+	client, err := ssh.Dial("tcp", address+":22", config)
 	if err != nil {
 		log.Fatal("Failed to dial: ", err)
 	}
@@ -95,8 +94,8 @@ func connectHandler(w http.ResponseWriter, r *http.Request){
 	}
 	fmt.Println(b.String())
 
-    t, _ := template.ParseFiles("connect.html")
-    t.Execute(w,nil)
+	t, _ := template.ParseFiles("connect.html")
+	t.Execute(w, nil)
 }
 
 func main() {
